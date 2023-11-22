7608795ba8802a286b684253d089d0ae5a9320b7
bless12ahadjie <blaise0ahadjie@gmail.com>
2023-11-22 20:01:23 +0000

    Navbar component

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {NavChildComponent} from '../nav-child/nav-child.component'
+import { LogoComponent } from '../logo/logo.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule,NavChildComponent],
+  imports: [CommonModule,NavChildComponent,LogoComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
