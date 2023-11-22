e738fcbdada721cbc168b0abbee5e95ac217181b
bless12ahadjie <blaise0ahadjie@gmail.com>
2023-11-22 20:10:27 +0000

    Radial yellowish component in the Hero section

---
+++ src/app/radial/radial.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-radial',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './radial.component.html',
+  styleUrl: './radial.component.css'
+})
+export class RadialComponent {
+
+}
