c9519aff38c92283de650ba8d8bc258f67cfc561
bless12ahadjie <blaise0ahadjie@gmail.com>
2023-11-22 21:03:30 +0000

    app modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { RadialComponent } from './radial/radial.component';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,NavbarComponent],
+  imports: [CommonModule, RouterOutlet,NavbarComponent,RadialComponent,
+    HeroSectionComponent,
+  
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
