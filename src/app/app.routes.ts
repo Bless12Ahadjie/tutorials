83a564c690710dd9eb1373884ac466d306816384
Bless Ahadjie <89154198+Bless12Ahadjie@users.noreply.github.com>
2024-01-16 18:52:39 +0000

    fixing routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ServiceSectionComponent } from './service-section/service-section.component';
 
 export const routes: Routes = [
-
+    {'path': '' , component: HomepageComponent},
+    // {'path': 'service' , component: ServiceSectionComponent},
     
 ];
--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RadialComponent } from '../radial/radial.component';
+import { HeroSectionComponent } from '../hero-section/hero-section.component';
+import { ServiceSectionComponent } from '../service-section/service-section.component';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,
+            RadialComponent,
+            HeroSectionComponent,
+            ServiceSectionComponent
+
+  ],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
